528334e7322b5bc1ffb4c393d9d463308cf87f29
Leo Dickens <dickens.leo@gmail.com>
2021-12-16 20:57:14 -0600

    concat arrays base on prompts

--- PswrdGen03.js
+++ PswrdGen03.js
@@ -11,23 +11,38 @@ function writePassword() {
 }
 
 function generatePassword(){
+  
+  //prompt vars
   var numOfChar = prompt("enter the length of password");
-
-  if (!(numOfChar >= 8 && numOfChar < 128)) {
-    return(alert("You must enter a value between 8 and 128!"));
-    }
-
   var lowPrompt = confirm("Should your password contain lowercase?");
   var upPrompt = confirm("Should your password include uppercase?");
   var numPrompt = confirm("Should your password include numbers?");
   var specPrompt = confirm("Should your password include special characters?");
+  
+  //value type arrays
   var specialChar = ["`","~","!","@","#","$","$","%","^","&","*","(",")","-","_","=","-","[","{","]","}",";",":","'",",","<",".",">","/","?","|",];
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var numericChar = ["0","1","2","3","4","5","6","7","8","9"];
-
-
+  var combinedSelections = []
   
+  if (!(numOfChar >= 8 && numOfChar < 128)) {
+    return(alert("You must enter a value between 8 and 128!"));
+    }
+  if (lowPrompt = true) {
+    combinedSelections = combinedSelections.concat(lowerCase);
+  }
+  if (upPrompt = true) {
+    combinedSelections = combinedSelections.concat(upperCase);
+    }
+  if (numPrompt = true) {
+    combinedSelections = combinedSelections.concat(numericChar);
+    }
+  if (specPrompt = true) {
+    combinedSelections = combinedSelections.concat(specialChar);
+    }
+  
+
   //based on user confirmation pick the correct arrays and concatenate them and store that concat array in new    variable
   //loop num times
   //generate random num for concat array and grab the item
