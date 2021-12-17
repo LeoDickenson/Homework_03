f5fa7cd18aa2460c5beb184f0cba10a487c075bf
Leo Dickens <dickens.leo@gmail.com>
2021-12-16 20:42:49 -0600

    added arrays

--- PswrdGen03.js
+++ PswrdGen03.js
@@ -12,16 +12,22 @@ function writePassword() {
 
 function generatePassword(){
   var numOfChar = prompt("enter the length of password");
-   if (!(numOfChar >= 8 && numOfChar < 128)) {
-   return(alert("You must enter a value between 8 and 128!"));
-   }
+
+  if (!(numOfChar >= 8 && numOfChar < 128)) {
+    return(alert("You must enter a value between 8 and 128!"));
+    }
+
   var lowPrompt = confirm("Should your password contain lowercase?");
   var upPrompt = confirm("Should your password include uppercase?");
-  var numPrompt = confirm("Should your password include numbers?")
-  var specPrompt = confirm("Should your password include special characters?") 
- 
-  // store all the values in form of arrays
-  // var numeric = [0,1,2,3...]
+  var numPrompt = confirm("Should your password include numbers?");
+  var specPrompt = confirm("Should your password include special characters?");
+  var specialChar = ["`","~","!","@","#","$","$","%","^","&","*","(",")","-","_","=","-","[","{","]","}",";",":","'",",","<",".",">","/","?","|",];
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numericChar = ["0","1","2","3","4","5","6","7","8","9"];
+
+
+  
   //based on user confirmation pick the correct arrays and concatenate them and store that concat array in new    variable
   //loop num times
   //generate random num for concat array and grab the item
